f41d734212b1787ab23038b42a79914ea2aceb0f
Cu63 <scube.str@gmail.com>
2022-09-20 21:54:20 +0300

    fix

--- scraper.py
+++ scraper.py
@@ -3,9 +3,6 @@
 # -Поиск по файлу(Мур)
 # -Обработка ошибок(Kir)
 #
-#
-#
-#
 
 from bs4 import BeautifulSoup
 import re
@@ -16,19 +13,14 @@ from urllib.error import HTTPError, URLError
 
 def get_page(url):
     html = urlopen(url)
-    print(html)
     bs = BeautifulSoup(html, 'html.parser')
     return bs
 
 
-def create_books_list(bs):
+def create_books_list(bs, table):
     prefix = 'https://www.piter.com'
-    titlelist = []
-    authorlist = []
-    pricelist = []
-    urllist = []
     for book in bs.find_all('div', {'class': re.compile(
-        '(grid-3 prod-block book-block )+(clear-class[0-9].)*')}):
+        '(grid-3 prod-block).*')}):
         title = book.find('span', {'class': 'title'})
         if title != None:
             title = title.text
@@ -44,22 +36,10 @@ def create_books_list(bs):
 #        print('Author: %s' % author)
 #        print('Price: %s' % price)
 #        print('Url: %s' % url)
-        titlelist.append(title)
-        authorlist.append(author)
-        pricelist.append(price)
-        urllist.append(price)
-
-    dataFrame1 = {'Title': titlelist,
-                  'author': authorlist,
-                  'price': pricelist,
-                  'url': urllist}
-
-    dataFrame2 = pd.DataFrame(dataFrame1)
-    print(dataFrame2)
-
-
-
-
+        table['Title'].append(title)
+        table['Author'].append(author)
+        table['Price'].append(price)
+        table['Url'].append(price)
 
 
 
@@ -72,20 +52,29 @@ def main():
                   'dom-byt-dosug', 'detskaya-literatura-igry',
                   'iskusstvo-i-kultura', 'prochee']
     for cat in categories:
+        table = {'Title': [], 'Author': [], 'Price': [], 'Url': []}
+
+# start page
         page_num = 1
         print('*' * 50)
         url = '%s%s?page=%d%s' % (prefix, cat, page_num, suffix)
         print('page %d' % page_num)
         print(url)
 
+# get html page
         bs = get_page(url)
         while True:
-            books_list = create_books_list(bs)
+            create_books_list(bs, table)
+            # increase page num
             page_num += 1
             print('page %d' % page_num)
+            # url of next
             url = '%s%s?page=%d%s' % (prefix, cat, page_num, suffix)
             bs = get_page(url)
+            # check last page
             if bs.find('a', {'class': 'nav'}, text='Следующая')['href'] == '':
+                frame = pd.DataFrame(table)
+                print(frame)
                 break
 
 
